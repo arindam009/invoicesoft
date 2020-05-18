58a0cad6b4d0e055ee76e2acbb257ff9cb659be3
arindam009 <arindam@winsomeitsolutions.com>
2020-05-18 14:24:10 +0530

    first commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome invoicesoft</title>
+
+	
+</head>
+<body>
+
+<div>
+<h3>Invoice Home</h3>
+	
+</div>
+
+</body>
+</html>
